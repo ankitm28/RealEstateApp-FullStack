f3e3562e59e065ba7dbf6f3ac469a197ed0eea3e
Ankit Mistry <ahmistry02@gmail.com>
2025-08-17 12:34:45 -0400

    added client

---
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import HeroSection from "./HeroSection";
+import FeaturesSection from "./FeaturesSection";
+import DiscoverSection from "./DiscoverSection";
+
+const Landing = () => {
+  return (
+    <div>
+      <HeroSection />
+      <FeaturesSection />
+      <DiscoverSection />
+    </div>
+  );
+};
+
+export default Landing;
